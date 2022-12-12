e25fc29046196d6e6358d9e1522e22cbc0da9329
Walter Torricos <ticot55@gmail.com>
2022-12-12 19:08:44 -0400

    Add serilog.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,32 @@
+using System.Globalization;
 using BlazorNullReferenceWithSerilog;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
+using Serilog.Events;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.BrowserConsole(
+        restrictedToMinimumLevel: LogEventLevel.Information,
+        outputTemplate: "[{Level:u3}] {Message:lj}{NewLine}{Exception}",
+        CultureInfo.InvariantCulture)
+    .CreateLogger();
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+try
+{
+    WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+    builder.RootComponents.Add<App>(selector: "#app");
+    builder.RootComponents.Add<HeadOutlet>(selector: "head::after");
 
-await builder.Build().RunAsync();
+    builder.Logging.AddSerilog();
+
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+    await builder.Build().RunAsync();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, messageTemplate: "An exception occurred while creating the WASM host");
+    throw;
+}
