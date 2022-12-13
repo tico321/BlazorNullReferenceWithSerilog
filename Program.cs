999ab72f6adfc0a4d9fb8431722fcd5d9238d434
Walter Torricos <ticot55@gmail.com>
2022-12-13 10:10:56 -0400

    Reproduce the issue.

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@ using System.Globalization;
 using BlazorNullReferenceWithSerilog;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Events;
 
+// If we comment this line the app runs fine
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.BrowserConsole(
@@ -12,6 +14,7 @@ Log.Logger = new LoggerConfiguration()
         outputTemplate: "[{Level:u3}] {Message:lj}{NewLine}{Exception}",
         CultureInfo.InvariantCulture)
     .CreateLogger();
+/////////////////////////////////////////////////////////////////////////
 
 try
 {
@@ -23,10 +26,41 @@ try
 
     builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-    await builder.Build().RunAsync();
+    builder.Services
+        .AddDbContextFactory<ADbContext>(opts =>
+        {
+            _ = opts
+                .UseInMemoryDatabase(databaseName: "PrototypeDatabase")
+                //.EnableSensitiveDataLogging()
+                .LogTo(Log.Information, LogLevel.Information);
+        });
+
+    WebAssemblyHost app = builder.Build();
+
+    // This block seems to be part of the problem, if we comment this block the code runs fine
+    ADbContext dbContext = app.Services.GetRequiredService<ADbContext>();
+    dbContext.Entities.Add(new EntityA());
+    await dbContext.SaveChangesAsync(CancellationToken.None);
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    await app.RunAsync();
 }
 catch (Exception ex)
 {
     Log.Fatal(ex, messageTemplate: "An exception occurred while creating the WASM host");
     throw;
+}
+
+public class ADbContext : DbContext
+{
+    public ADbContext(DbContextOptions<ADbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<EntityA> Entities => Set<EntityA>();
+}
+
+public class EntityA
+{
+    public int Id { get; set; }
 }
